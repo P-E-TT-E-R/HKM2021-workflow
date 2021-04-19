136e4cd1393d34bde91895ff137cf59ba0c4bdc2
P-E-TT-E-R <37306965+P-E-TT-E-R@users.noreply.github.com>
2021-04-19 08:40:39 +0200

    Some stuff

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Room, Event, Project
 # Register your models here.
+
+@admin.register(Room)
+class RoomControl(admin.ModelAdmin):
+  list_display = [field.name for field in
+Room._meta.get_fields()]
+
+@admin.register(Event)
+class EventControl(admin.ModelAdmin):
+  list_display = [field.name for field in
+Event._meta.get_fields()]
+
+@admin.register(Project)
+class ProjectControl(admin.ModelAdmin):
+  list_display = [field.name for field in
+Project._meta.get_fields()]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Schedule(models.Model):
+    owner = models.CharField(max_length=32, null=False)
+    """time = models.DateTimeField()
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=255)
+    data = models.CharField(max_length=255)"""
+    data_json = models.CharField(max_length=10000, default="{}")
+    
+class Room(models.Model):
+    name = models.CharField(max_length=32, null=False)
+    permission = models.CharField(max_length=32, null=True)
+    description = models.CharField(max_length=255, null=True)
+    availableFrom = models.TimeField(null=True)
+    availableTo = models.TimeField(null=True)
+
+class Project(models.Model):
+    name = models.CharField(max_length=32, null=False)
+    permission = models.CharField(max_length=32, null=True)
+    description = models.CharField(max_length=255, null=True)
+    waiting_json = models.CharField(max_length=10000, default="{}")
+    in_progress_json = models.CharField(max_length=10000, default="{}")
+    finished_json = models.CharField(max_length=10000, default="{}")
+
+class Event(models.Model):
+    name = models.CharField(max_length=32, null=False)
+    permission = models.CharField(max_length=32, null=True)
+    description = models.CharField(max_length=255, null=True)
+    date = models.DateTimeField(null=True)
+    participating = models.CharField(max_length=10000, default="{}")
+    data_json = models.CharField(max_length=10000, default="{}")
